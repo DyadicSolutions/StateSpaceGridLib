5e02cd994942eaaa64c57a7328305fd656baf9ce
maggym00 <41804925+maggym00@users.noreply.github.com>
2025-05-11 16:05:44 +0100

    Add stubs for measures module and unit test
    Do some checking for valid input.
    Functionality still to come.

---
+++ StateSpaceGridLib/statespacegrid/measure.py
@@ -0,0 +1,133 @@
+"""Module containing definitions of trajectory measures, and a dataclass to store all the measures for a trajectory
+
+Measures are the various properties calculated from a trajectory or set of trajectories. They are as follows:
+
+- mean trajectory duration: The mean duration of all trajectories provided to get_measures or get_mean_duration
+- mean number of events: The mean number of events in the trajectories provided to get_measures or get_mean_num_events
+- mean number of visits: The mean number of events in a state across all trajectories provided to get_measures or get_mean_num_visits. Consecutive events in the same state count as a single visit
+- mean cell range: The mean number of unique states visited by the trajectories provided to get_measures or get_mean_cell_range
+- total cell range: The total number of unique states visited across all trajectories provided to get_measures or get_total_cell_range
+- mean event duration: The mean duration of an event for a single trajectory, averaged across all trajectories provided to get_measures or get_mean_event_duration
+- mean visit duration: The mean duration of a visit to a state for a single trajectory, averaged across all trajectories provided to get_measures or get_mean_visit_duration. Consecutive events in the same state count as a single visit
+- mean state duration: The mean time spent in a state for a single trajectory, averaged across all trajectories provided to get_measures or get_mean_state_duration. This does not include states not visited in the state space.
+- mean dispersion: Mean dispersion of a trajectory, averaged across all trajectories provided to get_measures or get_mean_dispersion.
+
+Typical usage example:
+
+    traj = trajectory.Trajectory(
+                x_range=["bad", "ok", "good"],
+                y_range=[0, 1, 2],
+                states=[("ok", 1), ("bad", 0), ("bad", 1), ("bad", 2), ("ok", 2), ("good", 2), ("good", 1), ("good", 0), ("ok", 0)],
+                times=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                )
+
+    print(measure.get_measures(traj))
+
+"""
+from dataclasses import dataclass
+from typing import List, Tuple
+from itertools import chain
+
+from statespacegrid.trajectory import Trajectory
+
+@dataclass()
+class Measures:
+    mean_trajectory_duration: float
+    mean_number_of_events: float
+    mean_number_of_visits: float
+    mean_cell_range: float
+    total_cell_range: int
+    mean_event_duration: float
+    mean_visit_duration: float
+    mean_state_duration: float
+    mean_dispersion: float
+
+def validate_trajectories(*trajs: Trajectory):
+    """Checks that state spaces are consistent across all provided trajectories"""
+
+    if len(trajs) == 0:
+        raise ValueError("You must provide at least 1 trajectory")
+
+    x_ranges = set(chain(*(traj.state_space.x_range for traj in trajs)))
+    if not all(map(lambda traj: set(traj.state_space.x_range) == x_ranges, trajs)):
+        raise ValueError("The state spaces of all provided trajectories must match")
+
+    y_ranges = set(chain(*(traj.state_space.y_range for traj in trajs)))
+    if not all(map(lambda traj: set(traj.state_space.y_range) == y_ranges, trajs)):
+        raise ValueError("The state spaces of all provided trajectories must match")
+
+    first_traj_x_range = trajs[0].state_space.x_range
+    if not all(map(lambda traj: all(len(set(comp)) == 1 for comp in zip(first_traj_x_range, traj.state_space.x_range)), trajs[1:])):
+        raise ValueError("The order of states should be the same in all state spaces in the provided trajectories")
+
+    first_traj_y_range = trajs[0].state_space.y_range
+    if not all(map(lambda traj: all(len(set(comp)) == 1 for comp in zip(first_traj_y_range, traj.state_space.y_range)), trajs[1:])):
+        raise ValueError("The order of states should be the same in all state spaces in the provided trajectories")
+
+def _get_mean_trajectory_duration(*trajs: Trajectory) -> float:
+    pass
+def get_mean_trajectory_duration(*trajs: Trajectory) -> float:
+    validate_trajectories(*trajs)
+    return _get_mean_trajectory_duration(*trajs)
+
+def _get_mean_num_events(*trajs: Trajectory) -> float:
+    pass
+def get_mean_num_events(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_num_events(*trajs)
+
+def _get_mean_num_visits(*trajs: Trajectory) -> float:
+    pass
+def get_mean_num_visits(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_num_visits(*trajs)
+
+def _get_mean_cell_range(*trajs: Trajectory) -> float:
+    pass
+def get_mean_cell_range(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_cell_range(*trajs)
+
+def _get_total_cell_range(*trajs: Trajectory) -> int:
+    pass
+def get_total_cell_range(*trajs: Trajectory) -> int:
+	validate_trajectories(*trajs)
+	return _get_total_cell_range(*trajs)
+
+def _get_mean_event_duration(*trajs: Trajectory) -> float:
+    pass
+def get_mean_event_duration(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_event_duration(*trajs)
+
+def _get_mean_visit_duration(*trajs: Trajectory) -> float:
+    pass
+def get_mean_visit_duration(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_visit_duration(*trajs)
+
+def _get_mean_state_duration(*trajs: Trajectory) -> float:
+    pass
+def get_mean_state_duration(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_state_duration(*trajs)
+
+def _get_mean_dispersion(*trajs: Trajectory) -> float:
+    pass
+def get_mean_dispersion(*trajs: Trajectory) -> float:
+	validate_trajectories(*trajs)
+	return _get_mean_dispersion(*trajs)
+
+def get_measures(*trajs: Trajectory) -> Measures:
+    validate_trajectories(*trajs)
+    return Measures(
+        mean_trajectory_duration = _get_mean_trajectory_duration(*trajs),
+        mean_number_of_events = _get_mean_num_events(*trajs),
+        mean_number_of_visits=_get_mean_num_visits(*trajs),
+        mean_cell_range=_get_mean_cell_range(*trajs),
+        total_cell_range=_get_total_cell_range(*trajs),
+        mean_event_duration=_get_mean_event_duration(*trajs),
+        mean_visit_duration=_get_mean_visit_duration(*trajs),
+        mean_state_duration=_get_mean_state_duration(*trajs),
+        mean_dispersion=_get_mean_dispersion(*trajs)
+    )
---
+++ StateSpaceGridLib/statespacegrid/test/test_measures.py
@@ -0,0 +1,51 @@
+import unittest
+
+import statespacegrid.measure as measure
+import statespacegrid.trajectory as trajectory
+
+class TestMeasureCalculations(unittest.TestCase):
+
+    def test_empty_input(self):
+        with self.assertRaises(Exception) as context:
+            measure.get_measures()
+        self.assertTrue("You must provide at least 1 trajectory" in str(context.exception))
+
+    def test_mismatched_states(self):
+        traj1 = trajectory.Trajectory(
+            x_range=["bad", "ok", "good"],
+            y_range=[0, 1, 2],
+            states=[("ok", 1), ("bad", 0), ("bad", 1), ("bad", 2), ("ok", 2), ("good", 2), ("good", 1), ("good", 0), ("ok", 0)],
+            times=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            )
+        traj2 = trajectory.Trajectory(
+            x_range=[0, 1, 2],
+            y_range=["bad", "ok", "good"],
+            states=[(1, "ok"), (0, "bad"), (1, "bad"), (2, "bad"), (2, "ok"), (2, "good"), (1, "good"), (0, "good"), (0, "ok")],
+            times=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            )
+
+        with self.assertRaises(Exception) as context:
+            measure.get_measures(traj1, traj2)
+        self.assertTrue("The state spaces of all provided trajectories must match" in str(context.exception))
+
+    def test_misordered_states(self):
+        traj1 = trajectory.Trajectory(
+            x_range=["bad", "ok", "good"],
+            y_range=[0, 1, 2],
+            states=[("ok", 1), ("bad", 0), ("bad", 1), ("bad", 2), ("ok", 2), ("good", 2), ("good", 1), ("good", 0), ("ok", 0)],
+            times=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            )
+
+        traj2 = trajectory.Trajectory(
+            x_range=["bad", "ok", "good"],
+            y_range=[0, 2, 1],
+            states=[("ok", 1), ("bad", 0), ("bad", 1), ("bad", 2), ("ok", 2), ("good", 2), ("good", 1), ("good", 0), ("ok", 0)],
+            times=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            )
+
+        with self.assertRaises(Exception) as context:
+            measure.get_measures(traj1, traj2)
+        self.assertTrue("The order of states should be the same in all state spaces in the provided trajectories" in str(context.exception))
+
+if __name__ == "__main__":
+    unittest.main()
