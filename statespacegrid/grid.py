8e11707e3d70289e1d1bf4bfb2f436632a62d134
maggym00 <41804925+maggym00@users.noreply.github.com>
2025-07-12 14:35:58 +0100

    Finish off grid drawing
    Add axis labels and title, add colour support
    Relax trajectory offsetting so that things are
    simply shifted out from the middle, removing the
    requirement that they can't overlap at all.
    Change circle opacity to alpha = 0.7.
    Add arrows.

--- statespacegrid/grid.py
+++ statespacegrid/grid.py
@@ -14,10 +14,12 @@ Typical usage example:
 
 from statespacegrid.trajectory import Trajectory, validate_trajectories
 
-from matplotlib import patches, pyplot
+from matplotlib import patches, pyplot, colors
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 from collections import defaultdict
+from math import cos, sin, pi
+from random import random
 
 @dataclass
 class DataPoint:
@@ -34,9 +36,12 @@ class DataPoint:
     y: float
     radius: float
 
-    def to_circle(self) -> patches.Circle:
+    def to_circle(self, colour, first = False) -> patches.Circle:
         """Convert point into a matplotlib circle"""
-        return patches.Circle((self.x, self.y), self.radius, zorder=2)
+        if first:
+            return patches.Circle((self.x, self.y), self.radius, zorder=2, edgecolor=colour, facecolor='none', alpha = 0.7)
+        else:
+            return patches.Circle((self.x, self.y), self.radius, zorder=2, color=colour, alpha = 0.7)
 
 
 def _group_trajectory_points_by_state(trajectory_points_list: List[List[DataPoint]]) -> defaultdict:
@@ -53,27 +58,23 @@ def _group_trajectory_points_by_state(trajectory_points_list: List[List[DataPoin
 
 def _adjust_trajectory_points_list(trajectory_points_list: List[List[DataPoint]]):
     """
-    Adjust data points in list so they no longer overlap.
+    Adjust data points in list so they are less likely to overlap.
     Function both adjusts the radius so that it fits inside the state box
-    and moves points within the state box so they do not sit on top of each other
+    and moves points within the state box so they do not sit in the centre
     """
     data_points_grouped_by_state = _group_trajectory_points_by_state(trajectory_points_list)
     normalisation_factor = max(map(lambda points: sum(2*point.radius for point in points), data_points_grouped_by_state.values()))
     for state, points in data_points_grouped_by_state.items():
         for point in points:
             point.radius /= normalisation_factor
-        if len(points) > 1:
-            """
-            2 or more data points means we will have to adjust
-            Arrange points along the diagonal
-            """
-            half_length = sum(map(lambda point: point.radius, points))
-            center = (state[0]-(half_length), state[1]-(half_length))
-            for point in points:
-                center = (center[0]+point.radius, center[1]+point.radius)
-                point.x = center[0]
-                point.y = center[1]
-                center = (center[0]+point.radius, center[1]+point.radius)
+        center = state
+        for i, point in enumerate(points):
+            random_angle = (random() + i) * 2 * pi / len(points)
+            max_offset = 0.5 - point.radius
+            actual_offset = ((1+random())/2) * max_offset
+
+            point.x = center[0] + (actual_offset * cos(random_angle))
+            point.y = center[1] + (actual_offset * sin(random_angle))
 
 
 def _get_trajectory_points(*trajs: Trajectory) -> List[List[DataPoint]]:
@@ -91,21 +92,45 @@ def _get_adjusted_trajectory_points(*trajs: Trajectory) -> List[List[DataPoint]]
     return trajectory_points_list
 
 
-def draw(*trajs: Trajectory):
-    """Create a visualisation of the provided points on a state space grid"""
+def draw(*trajs: Trajectory, filename: str="ssg.png", colours: Optional[List[str]]=None,
+         xlabel: Optional[str]=None, ylabel: Optional[str]=None, title: Optional[str]=None):
+    """
+    Create a visualisation of the provided points on a state space grid
+
+    Keyword Arguments:
+    filename: File path for output grid visualisation
+    colours: List of colours for trajectories. If not provided,
+             a grid with multiple trajectories will assign each
+             trajectory a new colour randomly. Uses matplotlib
+             colors interface
+    xlabel: Axis label for the x axis
+    ylabel: Axis label for the y axis
+    title: Title for the plot
+    """
     validate_trajectories(*trajs)
 
     fig, ax = pyplot.subplots()
 
     trajectory_points_list = _get_adjusted_trajectory_points(*trajs)
 
-    for trajectory_points in trajectory_points_list:
+    # Figure out the different trajectory colours
+    colours_to_set = []
+    if colours is not None:
+        if len(colours) != len(trajs):
+            raise ValueError("The number of colours provided should match the number of trajectories")
+        colours_to_set=colours
+    else:
+        colour_list = list(colors.TABLEAU_COLORS.keys())
+        colours_to_set = [colour_list[i%len(colour_list)] for i in range(len(trajs))]
+
+    for trajectory_points, trajectory_colour in zip(trajectory_points_list, colours_to_set):
         # Add lines
         for point1, point2 in zip(trajectory_points, trajectory_points[1:]):
-            ax.plot((point1.x, point2.x), (point1.y, point2.y), "-ok", mfc='C0', mec='C0', zorder = 1)
+            ax.plot((point1.x, point2.x), (point1.y, point2.y), "-ok", mfc='k', mec='k', zorder = 1)
+            ax.arrow(point1.x, point1.y, (point2.x-point1.x)/2, (point2.y-point1.y)/2, head_width = 0.05, length_includes_head = True, fc='k')
         # Add circles
-        for point in trajectory_points:
-            ax.add_patch(point.to_circle())
+        for i, point in enumerate(trajectory_points):
+            ax.add_patch(point.to_circle(trajectory_colour, i==0))
 
     # Set grid size
     ax.set_xlim((-0.5, len(trajs[0].state_space.x_range)-0.5))
@@ -120,5 +145,14 @@ def draw(*trajs: Trajectory):
     ax.set_yticks([i-0.5 for i in range(len(trajs[0].state_space.y_range)+1)], minor=True)
     ax.grid(which="minor")
 
+    # Add axis labels
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
 
-    fig.savefig("ssg.png")
+    # Add figure title
+    if title is not None:
+        ax.set_title(title)
+
+    fig.savefig(filename)
