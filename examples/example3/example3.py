45364820272a92d78cf6a461d6f14247fc8689d9
maggym00 <41804925+maggym00@users.noreply.github.com>
2025-08-30 12:28:45 +0100

    Fix typo. Flesh out description

--- examples/example3/example3.py
+++ examples/example3/example3.py
@@ -8,7 +8,8 @@ import pandas as pd
 
 if __name__=="__main__":
    # read in the contents of a trj data file called example3.trj
-   # .trj files are basically csv files, but suing tabs instead of commas to separate variables
+   # .trj files are basically csv files, but using tabs instead of commas to separate variables
+   # we specify that the separator is a tab to read_csv with sep="\t"
    raw_data = pd.read_csv('example3.trj', sep="\t")
    # pass the columns of the trj file to the Trajectory object constructor
    # make sure to get rid of any NaN (Not a Number) values with dropna() and convert to a list with tolist()
